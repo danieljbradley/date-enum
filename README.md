caa5403fdfc15fc3012b101d6f95af2041c41481
meehanjohn <meehan6@illinois.edu>
2019-12-23 08:27:25 -0500

    First pass at date filter function

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # date-enum
- python library that enumerates dates between two dates at regular interval
+Subsets a dataframe on rows which overlap a given date range.
+
+# Usage:
+```
+from date_enum import date_filter
+test_df = pd.read_csv('data.csv')
+test_df_filtered = date_filter(
+  test_df,
+  start_date='3/6/2016',
+  end_date='5/9/2016'
+)
+
+>>> test_df_filtered.groupby('Race').count()
+                 ID  Start Date  End Date  Gender  Zip  Age
+Race
+Asian             4           4         3       4    4    4
+Black            12          12         2      12   12   12
+Hispanic          9           9         5       9    9    9
+Multi-race        6           6         3       6    6    6
+Some Other Race   8           8         2       8    8    8
+White             6           6         4       6    6    6
+
+>>> test_df_filtered.groupby('Gender').count()
+ID  Start Date  End Date  Zip  Age  Race
+Gender
+Female  15          15         6   15   15    15
+Male    30          30        13   30   30    30
+
+```
--- date_enum.py
+++ date_enum.py
@@ -1,67 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 11:51:07 2019
-
-@author: DBradley
-"""
-
 import pandas as pd
-import datetime as dt
-        
-path = 'C:\\Users\\DBradley\\OneDrive - Big Brothers Big Sisters Independence '
-path = path + 'Region\\Python\\date-enum\\'
-filename = 'data.csv'
+import numpy as np
 
-parse_dates = ['Start Date', 'End Date']
+# Input raw dataset, as well as start and end dates to filter on
+# End date defaults to today
+def date_filter(df, start_date, end_date=np.datetime64('today', 'D')):
 
-df = pd.read_csv(path+filename,
-                 #encoding = 'ISO-8859-1', 
-                 parse_dates=parse_dates)
+    # Return boolean check on whether a given date lies within the specified range
+    def date_check(test_date, start, end):
+        if test_date in pd.date_range(start=start, end=end):
+            return True
+        else:
+            return False
 
-df['End Date'] = df['End Date'].fillna(dt.date.today())
-
-# BONM means 'beginning of next month' 
-# date_range will only count full months, so I need to artificially extend 
-# date range to so that upper bound is included
-df['End Date_BONM'] = (df['End Date'] + pd.offsets.MonthBegin(n=1))
-
-########################################
-# generate active months for each match
-df.reset_index(drop=True, inplace=True)
-
-for i in df.index:
-    if i != 0: # append dates for subsequent rows onto df created in 'else'
-        # generate sequence of months
-        # during our meeting, I said that the enumerated date was the first 
-        # of the month.  I was mistaken - it is the last of the month
-        dt_range = pd.date_range(df['Start Date'].iloc[i],
-                                 df['End Date_BONM'].iloc[i],
-                                 freq='M')
-        # count number of months (for creating series of repeated index)
-        dt_range_size = dt_range.size
-        # convert index to pandas series that will be repeated for each month
-        idx = pd.Series(i)
-        idx_repeated = idx.repeat(dt_range_size)
-        dt_active_temp = pd.DataFrame(dt_range, index=idx_repeated)
-        dt_active = pd.concat([dt_active, dt_active_temp], axis=0)
-        
-    else: # start dataframe for first row
-        # generate sequence of months
-        dt_range = pd.date_range(df['Start Date'].iloc[i],
-                                 df['End Date_BONM'].iloc[i],
-                                 freq='M')
-        # count number of months (for creating series of repeated index)
-        dt_range_size = dt_range.size
-        # convert index to pandas series that will be repeated for each month
-        idx = pd.Series(i)
-        idx_repeated = idx.repeat(dt_range_size)
-        dt_active = pd.DataFrame(dt_range, index=idx_repeated)
-
-# rename month column
-dt_active = dt_active.rename(columns={0:'active_month'})
-#######################
-# merge active months with mf_grouped
-df_bymonth = pd.merge(dt_active, df, left_index=True, right_index=True)
-
-#df_bymonth.to_csv(path + 'df_bymonth.csv',encoding='latin1')
-df_bymonth.to_csv(path + 'df_bymonth.csv')
+    # Vectorize the boolean check to operate on an array
+    df_filter = np.vectorize(date_check)
+    # Create boolean array on the match start dates
+    start_mask = df_filter(df['Start Date'], start_date, end_date)
+    # Create boolean array on the match end dates
+    end_mask = df_filter(df['End Date'], start_date, end_date)
+    # Subset dataframe values where the Start Date OR End Date are within the given range
+    df = df[start_mask | end_mask]
+    # Return filtered dataframe
+    return(df)
